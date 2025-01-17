0ebc79d2423aa9afc7e45d9bba24a231c8cc5d78
Naman Rai <namanrai567484@gmail.com>
2025-01-17 11:23:14 +0530

    classes and constructor

---
+++ classes_Objects/app.js
@@ -0,0 +1,43 @@
+"use strict";
+// Classes and Objects
+// Class definition
+// Constructors
+// Access modifiers (public, private, protected)
+// Readonly properties
+// Optional properties
+// Parameter properties
+// Getters and setters
+// Static members
+// Abstract classes and methods
+//  1
+// class Device{
+//     name= "lg"
+//     price= 12000
+//     category= "digital"
+// }
+// let d1 =  new Device()
+// let d2 =  new Device()
+// class bottle{
+//     radius = 22
+//     price = 4567
+// }
+// let b1 = new bottle()
+//Constructor
+// class bottle {
+//     constructor(public name: string, public price: number){}
+// }
+// let b1 = new bottle("milton",299)
+// class humanMaker{
+//     color = "white"
+//     constructor(public name: string, public age: number){}
+// }
+// let h1 = new humanMaker("naman",22)
+class family {
+    constructor(name, members) {
+        this.name = name;
+        this.members = members;
+    }
+}
+let fam = new family("naman", 2);
+fam.name = "neha";
+// let fam1  = new family("neha",22)
